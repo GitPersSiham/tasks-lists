a97d5bbe9ecf8ac2c3d09c981aa7e0c14306d6f6
GitPersSiham <lahouali.siham@outlook.fr>
2020-11-10 14:31:27 +0100

    ajout methode post dans authcontroller .Closes #15

--- src/router.ts
+++ src/router.ts
@@ -2,19 +2,21 @@ import express from 'express';
 
 import MainController from './controllers/mainController';
 import AuthController from './controllers/authController';
+import bodyparser from 'body-parser';
 
 const router: express.Router = express.Router();
 
+const bodyParser = bodyparser.urlencoded({extended: true});
 
 // on définit des routes
 router.get('/', MainController.index);
 
 router.route('/login')
   .get(AuthController.getLogin)
- 
+  .post(bodyParser, AuthController.postLogin);
 
 router.route('/signin')
   .get(AuthController.getSignin)
-  
+  .post(bodyParser, AuthController.postSignin);
 
 export default router;
