9eec72e6e9a0531ae43a4dc73f50786eb2a0ff9b
GitPersSiham <lahouali.siham@outlook.fr>
2020-11-12 14:26:34 +0100

    ajout swagger coté back .Closes #35

--- src/router.ts
+++ src/router.ts
@@ -4,11 +4,23 @@ import MainController from './controllers/mainController';
 import AuthController from './controllers/authController';
 import bodyparser from 'body-parser';
 
+
 const router: express.Router = express.Router();
 
 const bodyParser = bodyparser.urlencoded({extended: true});
 
 // on définit des routes
+/** 
+ * @swagger
+ * /:
+ * get:
+ *   description : homepage donne la possiblité à un utilistateur de creer un compte ou de se connecter
+ *   response:
+ *   '200':
+ *     description : A success response
+*/ 
+
+
 router.get('/', MainController.index);
 
 router.route('/login')
